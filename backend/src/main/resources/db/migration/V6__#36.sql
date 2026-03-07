747bc0ad3636a565f378f8f9586db4f8233f5e08
Isacco <isaccotrevisan@gmail.com>
2026-03-07 10:21:49 +0100

    fix sql

--- backend/src/main/resources/db/migration/V6__#36.sql
+++ backend/src/main/resources/db/migration/V6__#36.sql
@@ -1,8 +1,8 @@
+alter table costi_retroilluminazione
+    rename to listino_retroilluminazione;
 alter table listino_retroilluminazione
     add column ricarico_percentuale numeric(19, 2);
 alter table listino_retroilluminazione
     add column prezzo numeric(19, 2);
 alter table listino_retroilluminazione
     add column descrizione text;
-alter table costi_retroilluminazione
-    rename to listino_retroilluminazione;
